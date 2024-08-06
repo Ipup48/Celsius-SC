529101c44710ed6992758e04be91ed917923b5b0
Ipup48 <674259022@webmail.npru.ac.th>
2024-08-06 12:03:20 +0700

    celsius Sc commit

---
+++ Celsius.java
@@ -0,0 +1,16 @@
+import java.util.Scanner ;
+    public class Celsius {
+        public static void main (String args []){
+            Scanner sc = new Scanner(System.in);
+            int mySalary = 2000;
+            System.out.print("Enter Fahrenheit degree: ");
+            float fahrenheit = sc.nextFloat();
+            float celsius = (fahrenheit - 32)* 5 / 9f;
+            double roundedCelsius = Math.round(celsius * 100.0) / 100.0;
+            System.out.println(fahrenheit +" Fahrenheit is equal to " + roundedCelsius +" Celsius.");
+            sc.close();
+        }
+    
+        
+    }
+
--- Fahrenheit.java
+++ Fahrenheit.java
@@ -1,7 +1,7 @@
 public class Fahrenheit {
-    public static void main (String agrs[]){
-        int fahrenheit = 80 ;
+    public static void main (String args[]){
+        float fahrenheit = 80.0f ;
         double celsius = (5.0/9.0) * (fahrenheit-32);
-        System.out.println(celsius);
+        System.out.println(fahrenheit + " Fahrenheit is equal to " + celsius + " celsius");
     }
 }
