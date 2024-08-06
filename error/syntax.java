376669a83a40b74d05b45d5e203512ce11bd4c1d
Ipup48 <674259022@webmail.npru.ac.th>
2024-08-06 12:18:28 +0700

    Delete error directory

--- error/syntax.java
+++
@@ -1,8 +0,0 @@
-public class syntax {
-    public static void main(String[] args) {
-        System.out.println("Hello World"); 
-        System.out.println(4/2); 
-        System.out.println(4.0/100);   
-        
-    }
-}
